5da92b3eb5951a54d8af9bb52c4bd8419f58f811
DERMANRAY <rizkydermawan12345@gmail.com>
2025-01-18 12:01:10 +0700

    Update CetakBilGenapForLoop.js

--- CetakBilGenapForLoop.js
+++ CetakBilGenapForLoop.js
@@ -3,7 +3,7 @@
  */
 
 
-let total = 0 //inisialisasi var total dengan nilai 0
+let total = 0 //inisialisasi variabel total dengan nilai 0
 
 for (let i = 1; i <= 50; i++) { //loop sebanyak 1 sampai 50
     if (i % 2===0) { // cek apakah i = bil genap
